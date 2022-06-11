160c1014f81af76e9628ed2b6b41bf408774c167
navjotsingh96 <navjot.singh96@web.de>
2022-06-11 11:52:30 +0200

    loading animation if user will saved to friestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 
@@ -46,7 +49,10 @@ import { AngularFireModule } from '@angular/fire/compat';
     MatDatepickerModule,
     MatNativeDateModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from 'src/models/user.class';
 
 @Component({
@@ -11,20 +13,36 @@ export class DialogAddUserComponent implements OnInit {
 
   user = new User();
   birthDate: Date;
-  constructor(private firestore: AngularFirestore) { }
+  loading = false;
+  private _snackBar: any;
+
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>,private snackBar: MatSnackBar,
+    
+    private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
   }
   save() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('Current User is', this.user);
+    this.loading = true;
+
     this.firestore
       .collection('user')
       .add(this.user.toJSON())
       .then((result: any) => {
+        this.loading = false;
         console.log('Adding user finsihed', result);
-
+        this.dialogRef.close();
       })
 
   }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+   /*  openSnackBar() {
+      this.snackBar.open('User is Added');
+    
+  } */
 }
