beeb2525bec60549d6869d75e6359b70fcfcbb38
navjotsingh96 <navjot.singh96@web.de>
2022-06-15 13:17:26 +0200

    showed user in HTML code from DB

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getMatInputUnsupportedTypeError } from '@angular/material/input';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,17 +10,30 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
-  userId ='';
-  
-  constructor(private route: ActivatedRoute) { }
+  userId = '';
+  user :User = new User;
+
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     // to get URL in this case ID 
-    this.route.paramMap.subscribe( paramMap =>{
+    this.route.paramMap.subscribe(paramMap => {
       this.userId = paramMap.get('id');
       console.log('Got id', this.userId);
-  
-    })
-  }
+      this.getUser();
+    });
 
+  }
+  
+  getUser() {
+    this.firestore
+      .collection('users')
+      .doc(this.userId)
+      .valueChanges()
+      .subscribe((user) => {
+        this.user = new User(user);
+        console.log('Current user', this.user);
+        
+      })
+  }
 }
